c00b7c5a6aaa7b64d6f66f4f0f28eba6d273516e
MarceloYudis <marceloyudisortiz@gmail.com>
2022-11-13 10:44:54 -0300

    correccion del async await

--- firstAPI/codigo.js
+++ firstAPI/codigo.js
@@ -1,32 +1,38 @@
 document.write("lol")
 const API = "https://api.thecatapi.com/v1/images/search"
 
-const botonsito = document.getElementById("BOTOM")
-botonsito.addEventListener("click",invocarGatitos())
-
-function invocarGatitos() {
-fetch(API)
-.then(response => response.json())
-.then(data => {
-    const IMGAgatito = data[0].url;
-    const imagen = document.querySelector('img');
-    imagen.src = IMGAgatito;
-})
-}
+const botonsito = document.getElementById("BOTOM");
 
 
 async function traerGatitos(apiURL) {
-    const respuesta = await fetch(apiURL);
+    const respuesta = await fetch(API);
     const data = await respuesta.json();
 
-    return data[0].url;
+   const gatetes = await data[0].url;
+   const imagen = await document.querySelector('img');
+        imagen.src = gatetes;
 }
 
-const mostrarGatitos = async(apiURL) => {
+const mostrarGatitos = async(url) => {
     try {
-        const gatetes = await traerGatitos(`${apiURL}`);
+        const gatetes = await traerGatitos(url);
+        const imagen = document.querySelector('img');
+        imagen.src = gatetes;
          
     } catch (error) {
 
     }
 }
+
+botonsito.addEventListener("click",traerGatitos(API));
+
+// function invocarGatitos() {
+// fetch(API)
+// .then(response => response.json())
+// .then(data => {
+//     const IMGAgatito = data[0].url;
+//     const imagen = document.querySelector('img');
+//     imagen.src = IMGAgatito;
+// })
+// }
+
