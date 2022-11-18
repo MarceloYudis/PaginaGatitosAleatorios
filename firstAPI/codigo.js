aa25e18828f4bf36c82f2741758aeebd8dc6e558
MarceloYudis <marceloyudisortiz@gmail.com>
2022-11-17 22:12:54 -0300

    lectura de michis favoritos

--- firstAPI/codigo.js
+++ firstAPI/codigo.js
@@ -1,8 +1,9 @@
 document.write("lol")
 const API = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_ehGicg6RazPr5LRt68uOav3QHOvJcsqWQb6vjIgfQG80ZZb5Z0Aog8vh2PBKtRr5"
+const favorites_API = "ttps://api.thecatapi.com/v1/favourites?limit=3api_key=live_ehGicg6RazPr5LRt68uOav3QHOvJcsqWQb6vjIgfQG80ZZb5Z0Aog8vh2PBKtRr5";
 
 const botonsito = document.getElementById("BOTOM");
-
+const spanError = document.getElementById("error");
 
 async function traerGatitos(apiURL) {
     const respuesta = await fetch(API);
@@ -12,13 +13,28 @@ async function traerGatitos(apiURL) {
    const gatetes1 = await data[1].url;
    const gatetes2 = await data[2].url;
 
-   const imagen0 = await document.getElementById('img0');
-    imagen0.src = gatetes0;
+    if(respuesta.status !==200) {
+        spanError.innerHTML = "Hubo un error" + respuesta.status;
+    } else {
+        const imagen0 = await document.getElementById('img0');
+        imagen0.src = gatetes0;
+    
+        const imagen1 = await document.getElementById('img1');
+        imagen1.src = gatetes1;
+        const imagen2 = await document.getElementById('img2');
+        imagen2.src = gatetes2;
+    }
+}
 
-    const imagen1 = await document.getElementById('img1');
-    imagen1.src = gatetes1;
-    const imagen2 = await document.getElementById('img2');
-    imagen2.src = gatetes2;
+async function favoritosGatitos() {
+    const respuesta = await fetch(favorites_API);
+    const data = await respuesta.json();
+
+    const favGatito = await data[0].url;
+
+    if(respuesta.status !==200) {
+        spanError.innerHTML = "Hubo un error" + respuesta.status;
+     }else{}
 }
 
 
